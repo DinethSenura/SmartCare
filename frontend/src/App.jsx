ee2cbf9c452cf279cf6d3d2c2e36fbb5ffe1df02
Dineth Senura <100292025+DinethSenura@users.noreply.github.com>
2024-11-22 01:12:52 +0530

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
 
-function App() {
+const App = () => {
   return (
-    <div className='text-green-500'>
-      aaaaaaaaaa
+    <div className='mx-4 sm:mx-[10%]'>
+
+      <Routes>
+        <Route path='/' element={<Home />}/>
+      </Routes>
     </div>
   )
 }
